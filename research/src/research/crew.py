0c0cecc80038eca82c45f4f54c2398f0a3aa5ac0
mpazaryna <mpazbot@gmail.com>
2024-05-24 11:44:14 -0400

    task: add support for Ollama

--- research/src/research/crew.py
+++ research/src/research/crew.py
@@ -1,74 +1,80 @@
-from crewai import Agent, Crew, Process, Task
-from crewai.project import CrewBase, agent, crew, task
-from langchain_groq import ChatGroq
-from langchain_openai import ChatOpenAI
 import os
 
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from langchain_community.llms import Ollama
+from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
+
+
 @CrewBase
-class ResearchCrew():
-	"""Research crew"""
-	agents_config = 'config/agents.yaml'
-	tasks_config = 'config/tasks.yaml'
- 
-	def __init__(self):
-		agent_model = os.getenv('AGENT_MODEL')
-		if agent_model == 'ChatOpenAI':
-			self.llm = ChatOpenAI(
-				model="gpt-4o"
-				# model="gpt-4-turbo"
-			)
-		elif agent_model == 'ChatGroq':
-			self.llm = ChatGroq(
-				# model="mixtral-8x7b-32768"
-				# model="llama3-8b-8192"
-				# model="llama3-70b-8192"
-				model="gemma-7b-it"
-			)
-		else:
-			raise ValueError("Invalid AGENT_MODEL environment variable value")
+class ResearchCrew:
+    """Research crew"""
 
-	@agent
-	def researcher(self) -> Agent:
-		return Agent(
-			config=self.agents_config['researcher'],
-			# tools=[MyCustomTool()], # Example of custom tool, loaded on the beginning of file
-			verbose=True,
-			llm=self.llm,
-			# max_iter=4,
-		)
+    agents_config = "config/agents.yaml"
+    tasks_config = "config/tasks.yaml"
 
-	@agent
-	def reporting_analyst(self) -> Agent:
-		return Agent(
-			config=self.agents_config['reporting_analyst'],
-			verbose=True,
-			llm=self.llm,
-			# max_iter=4,
-		)
+    def __init__(self):
+        agent_model = os.getenv("AGENT_MODEL")
+        if agent_model == "ChatOpenAI":
+            self.llm = ChatOpenAI(
+                model="gpt-4o"
+                # model="gpt-4-turbo"
+            )
+        elif agent_model == "ChatGroq":
+            self.llm = ChatGroq(
+                # model="mixtral-8x7b-32768"
+                # model="llama3-8b-8192"
+                # model="llama3-70b-8192"
+                model="gemma-7b-it"
+            )
+        elif agent_model == "Ollama":
+            self.llm = Ollama(model="mistral")
+        else:
+            raise ValueError("Invalid AGENT_MODEL environment variable value")
 
-	@task
-	def research_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['research_task'],
-			agent=self.researcher(),
-			output_file='research.md'
-		)
+    @agent
+    def researcher(self) -> Agent:
+        return Agent(
+            config=self.agents_config["researcher"],
+            # tools=[MyCustomTool()], # Example of custom tool, loaded on the beginning of file
+            verbose=True,
+            llm=self.llm,
+            # max_iter=4,
+        )
 
-	@task
-	def reporting_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['reporting_task'],
-			agent=self.reporting_analyst(),
-			output_file='report.md'
-		)
+    @agent
+    def reporting_analyst(self) -> Agent:
+        return Agent(
+            config=self.agents_config["reporting_analyst"],
+            verbose=True,
+            llm=self.llm,
+            # max_iter=4,
+        )
 
-	@crew
-	def crew(self) -> Crew:
-		"""Creates the Research crew"""
-		return Crew(
-			agents=self.agents, # Automatically created by the @agent decorator
-			tasks=self.tasks, # Automatically created by the @task decorator
-			process=Process.sequential,
-			verbose=2,
-			# process=Process.hierarchical, # https://docs.crewai.com/how-to/Hierarchical/
-		)
+    @task
+    def research_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["research_task"],
+            agent=self.researcher(),
+            output_file="research.md",
+        )
+
+    @task
+    def reporting_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["reporting_task"],
+            agent=self.reporting_analyst(),
+            output_file="report.md",
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the Research crew"""
+        return Crew(
+            agents=self.agents,  # Automatically created by the @agent decorator
+            tasks=self.tasks,  # Automatically created by the @task decorator
+            process=Process.sequential,
+            verbose=2,
+            # process=Process.hierarchical, # https://docs.crewai.com/how-to/Hierarchical/
+        )
